af339e3f47a6b4d714b80cd4e2a51824b5f165b0
Klassikcat <shinjeongtae@gmail.com>
2021-10-07 01:10:34 +0900

    [edit] update requirement.txt

--- readability_app/config.py
+++ readability_app/config.py
@@ -7,33 +7,6 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET')
     DEBUG = False
 
-class DevelopmentConfig(Config):
-    # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://<id>:<password>@<server url host>:<server url port>/<db name>'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class TestingConfig(Config):
-    DEBUG = True
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://<id>:<password>@<server url host>:<server url port>/<db name>'
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class ProductionConfig(Config):
-    DEBUG = False
-    # uncomment the line below to use postgres
-    SQLALCHEMY_DATABASE_URI = 'mysql://<배포한 DB의 id>:<배포한 DB의 pw>@<배포한 server url host>/<배포한 DB name>'
-
-config_by_name = dict(
-    dev=DevelopmentConfig,
-    test=TestingConfig,
-    prod=ProductionConfig
-)
-
-key = Config.SECRET_KEY
-
 domain_list = {
     '연설': 1,
     '편지': 2,
